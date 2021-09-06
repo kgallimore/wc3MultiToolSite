b23180bc1cfc6dbcf04810221b87bed39e606950
kgallimore <72752967+kgallimore@users.noreply.github.com>
2021-09-05 23:43:38 -0400

    Init

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name;
+  let latestDownloadURL, latestVersion;
+  function getLatestDownload() {
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://war.trenchguns.com/publish/latest.yml", true);
+    request.send(null);
+    request.onreadystatechange = function () {
+      if (request.readyState === 4 && request.status === 200) {
+        latestVersion = request.responseText
+          .split("\n")[2]
+          .split(": ")[1]
+          .trim();
+        latestDownloadURL = latestVersion.replace(/\s/g, "%20");
+      }
+    };
+  }
+  getLatestDownload();
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    <a href="/publish/{latestDownloadURL}">Download {latestVersion}</a>
+  </p>
+</main>
+
+<style>
+</style>
